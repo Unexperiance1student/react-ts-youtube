716c30bd8f07a654d9c6c0c7efb31136af76d512
Vladislav <75188379+Unexperiance1student@users.noreply.github.com>
2023-03-09 04:40:04 +0300

    create navbar and add dop initial struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home, Search, Watch } from './pages';
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/search'
+          element={<Search />}
+        />
+        <Route
+          path='/watch/:id'
+          element={<Watch />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
